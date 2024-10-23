7dae0d6a173ce6637e7471f245b0bcf9addad318
blu3b3rr13s1512 <honeylattecrochet@gmail.com>
2024-10-23 20:05:58 +0800

    save

--- uploadfiles.py
+++ uploadfiles.py
@@ -3,9 +3,12 @@ import streamlit as st
 menu = st.sidebar.selectbox('Choose an option',['Upload Image','Upload Audio','Upload Video'])
 
 if menu == 'Upload Image':
+    selectoption = st.radio('Choose option',['Upload a Picture','Take a Picture'],horizontal=True)
     uploadimage = st.file_uploader("Upload your image here", type=["jgp","jpeg","png"])
     if uploadimage:
         st.image(uploadimage)
+if selectoption == 'Take a Picture':
+    camera = st.camera_input("Smile to the camera!")
 
 if menu == 'Upload Audio':
     uploadaudio = st.file_uploader("Upload your audio here", type = ['mp3','wav'])
